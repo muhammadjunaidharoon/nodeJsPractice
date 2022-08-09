d2eb6bdfc764bd429ed667da1dbe271ab405a0b8
Muhammad Junaid Haroon <105660716+muhammadjunaidharoon@users.noreply.github.com>
2022-08-09 19:17:42 +0500

    amendment in server.mjs file

--- server.mjs
+++ server.mjs
@@ -1,9 +1,14 @@
 import express from 'express'
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/home', (req, res) => {
+  res.send('I am at Home page!')
+})
+
+app.get('/profile', (req, res) => {
+  res.send('I am profile Page!')
 })
 
 app.listen(port, () => {
