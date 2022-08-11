f51874b8a037707a408c7093a603cf9aac7cb588
Muhammad Junaid Haroon <105660716+muhammadjunaidharoon@users.noreply.github.com>
2022-08-11 19:47:03 +0500

    update first server request

--- server.mjs
+++ server.mjs
@@ -1,7 +1,9 @@
 import express from 'express'
+import cors from 'cors';
 
 const app = express()
 const port = 3000
+app.use(cors());
 
 app.get('/home', (req, res) => {
   res.send('I am at Home page!')
